a257c11b32ef7c83d25507cadba557dc16d497fc
jmeyer <jmeyer@mimecast.com>
2018-02-06 19:15:35 +0000

    Added Solution

--- Bitset.cpp
+++ Bitset.cpp
@@ -4,20 +4,32 @@ using namespace CPPLondonUni;
 
 Bitset::Bitset(char bit) : bitfield{bit} {}
 
-bool Bitset::test(int idx) { return false; }
+bool Bitset::test(int idx) { return bitfield & (1 << idx); }
 
-bool Bitset::all() { return false; }
+bool Bitset::all() { return bitfield == static_cast<char>(0xFF); }
 
-bool Bitset::any() { return false; }
+bool Bitset::any() { return static_cast<unsigned char>(bitfield) > 0; }
 
-bool Bitset::none() { return false; }
+bool Bitset::none() { return bitfield == 0; }
 
-int Bitset::count() { return 0; }
+int Bitset::count() {
+	int ret;
+	for(int i = 0; i < 8; ++i)
+		if(test(i))
+			ret++;
+	return ret;
+}
 
-int Bitset::size() { return 0; }
+int Bitset::size() { return 8; }
 
-void Bitset::set(int idx) {}
+void Bitset::set(int idx) {
+	bitfield |= (1 << idx);
+}
 
-void Bitset::reset(int idx) {}
+void Bitset::reset(int idx) {
+	bitfield &= ~(1 << idx);
+}
 
-void Bitset::flip() {}
+void Bitset::flip() {
+	bitfield = ~bitfield;
+}
