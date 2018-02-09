38083d00bac9d90b0ade49bf4023987dc822fff6
Colin Newell <colin.newell@gmail.com>
2018-02-09 20:19:44 +0000

    Implemented exercise

--- Bitset.cpp
+++ Bitset.cpp
@@ -1,23 +1,41 @@
 #include "Bitset.h"
+#include <iostream>
 
 using namespace CPPLondonUni;
 
+
 Bitset::Bitset(char bit) : bitfield{bit} {}
 
-bool Bitset::test(int idx) { return false; }
+bool Bitset::test(int idx) { return (this->bitfield & (1<<idx)) > 0; }
 
-bool Bitset::all() { return false; }
+bool Bitset::all() { return this->bitfield == (char)0xff; }
 
-bool Bitset::any() { return false; }
+bool Bitset::any() {
+    return static_cast<unsigned char>(this->bitfield) > 0;
+}
 
-bool Bitset::none() { return false; }
+bool Bitset::none() { return this->bitfield == 0; }
 
-int Bitset::count() { return 0; }
+int Bitset::count() {
+    int count = 0;
+    for (int i = 0; i < 8; i++) {
+        if(this->test(i)) {
+            count++;
+        }
+    }
+    return count;
+}
 
-int Bitset::size() { return 0; }
+int Bitset::size() { return sizeof(this->bitfield)* 8; }
 
-void Bitset::set(int idx) {}
+void Bitset::set(int idx) {
+    this->bitfield = this->bitfield | (char)(1<<idx);
+}
 
-void Bitset::reset(int idx) {}
+void Bitset::reset(int idx) {
+    this->bitfield = this->bitfield & (char)~(1<<idx);
+}
 
-void Bitset::flip() {}
+void Bitset::flip() {
+    this->bitfield = ~this->bitfield;
+}
