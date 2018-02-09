417bd686fcb686bef81588c1c874f7e49d18a892
Colin Newell <colin.newell@gmail.com>
2018-02-09 20:28:10 +0000

    Cleaned up

--- Bitset.cpp
+++ Bitset.cpp
@@ -1,5 +1,4 @@
 #include "Bitset.h"
-#include <iostream>
 
 using namespace CPPLondonUni;
 
@@ -8,7 +7,7 @@ Bitset::Bitset(char bit) : bitfield{bit} {}
 
 bool Bitset::test(int idx) { return (this->bitfield & (1<<idx)) > 0; }
 
-bool Bitset::all() { return this->bitfield == (char)0xff; }
+bool Bitset::all() { return this->bitfield == static_cast<char>(0xff); }
 
 bool Bitset::any() {
     return static_cast<unsigned char>(this->bitfield) > 0;
@@ -19,7 +18,7 @@ bool Bitset::none() { return this->bitfield == 0; }
 int Bitset::count() {
     int count = 0;
     for (int i = 0; i < 8; i++) {
-        if(this->test(i)) {
+        if(test(i)) {
             count++;
         }
     }
@@ -29,11 +28,11 @@ int Bitset::count() {
 int Bitset::size() { return sizeof(this->bitfield)* 8; }
 
 void Bitset::set(int idx) {
-    this->bitfield = this->bitfield | (char)(1<<idx);
+    this->bitfield |= (1<<idx);
 }
 
 void Bitset::reset(int idx) {
-    this->bitfield = this->bitfield & (char)~(1<<idx);
+    this->bitfield &= ~(1<<idx);
 }
 
 void Bitset::flip() {
